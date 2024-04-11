c88b86500f2badc0806e8e90a34f742fec369d90
openglobe <openglobe@gmail.com>
2024-04-11 16:23:25 -0400

    restart

--- src/config/filters.js
+++
@@ -1,8 +0,0 @@
-/**
- * Add Eleventy filters here
- * https://www.11ty.dev/docs/filters/
-*/
-
-module.exports = {
-
-}
--- src/data/env.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-  return {
-    siteUrl: process.env.URL,
-    branch: process.env.BRANCH
-  };
-};
